233dd569b53cd838d5993025fbc895804606fdfb
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-05-21 16:42:38 -0400

    basic grid setup complete

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const grid = document.querySelector(".grid");
+    let squares = Array.from(document.querySelectorAll(".grid div"));
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start-button");
+    const width = 10;
+    
+
+});
